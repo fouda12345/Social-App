5b9d89166892dbbd8f760cd963a8202087e0c013
fouda12345 <ahmedfouda9.af@gmail.com>
2025-09-15 21:10:49 +0300

    renames

--- src/Modules/Auth/auth.services.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class AuthService {
-    constructor() { }
-    login = async (req, res, next) => {
-        return res.status(200).json({ message: "Login successful" });
-    };
-}
-exports.default = new AuthService();
